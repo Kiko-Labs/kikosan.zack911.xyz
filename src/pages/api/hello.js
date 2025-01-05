891441d673e410387dc5d04207ecd5dfd9752cc3
muhammadbinasim471 <muhammadbinasim471@gmail.com>
2025-01-05 08:14:32 +0000

    added a secret api thingy

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({ name: 'Joe...... JOE MAMA!!!! HAHAHAHAHHAHAHAHAHAHAHAHAHA. Btw how did u even come here? this was a secret place!' })
 }
