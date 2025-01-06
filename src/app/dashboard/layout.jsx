810deca00015d645edc0e4e2fccaa81e4f388a1e
muhammadbinasim471 <muhammadbinasim471@gmail.com>
2025-01-06 08:28:06 +0000

    Added layout.jsx

---
+++ src/app/dashboard/layout.jsx
@@ -0,0 +1,10 @@
+// app/dashboard/layout.jsx
+export default function DashboardLayout({ children }) {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        {children}
+      </div>
+    );
+  }
+  
